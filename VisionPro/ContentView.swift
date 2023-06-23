79441adc4359aa89348a51b6c8168acb7c5466b7
coiai <coiaichan@gmail.com>
2023-06-24 03:42:59 +0900

    Initial Commit

---
+++ VisionPro/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  VisionPro
+//
+//  Created by coiai on 2023/06/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionPro/VisionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProApp.swift
+//  VisionPro
+//
+//  Created by coiai on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
